437484abcd2f50af8a6e530e8963b95832faf496
zhizhanxue <zhanxue.zhi@17zuoye.com>
2020-09-28 17:18:46 +0800

    快慢指针求链表有环

---
+++ src/main/java/链表/单链表中圆环的开始节点_142/Solution.java
@@ -0,0 +1,58 @@
+package 链表.单链表中圆环的开始节点_142;
+
+import 链表.structure.ListNode;
+
+//给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+//
+// 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+//
+// 说明：不允许修改给定的链表。
+//
+//
+// 示例 1：
+//
+// 输入：head = [3,2,0,-4], pos = 1
+//输出：tail connects to node index 1
+//解释：链表中有一个环，其尾部连接到第二个节点。
+//
+//
+// 示例 2：
+//
+// 输入：head = [1,2], pos = 0
+//输出：tail connects to node index 0
+//解释：链表中有一个环，其尾部连接到第一个节点。
+//
+//
+// 示例 3：
+//
+// 输入：head = [1], pos = -1
+//输出：no cycle
+//解释：链表中没有环。
+//
+//
+// 进阶：
+//你是否可以不用额外空间解决此题？
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+
+        if (head == null || head.next == null) return null;
+
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+
+            if (fast == slow) {
+                slow = slow.next;
+                for (ListNode p = head; p != slow; p = p.next, slow = slow.next) ;
+                return slow;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return null;
+    }
+
+    //思路
+    //链表有环问题思路先看141.先使用快慢指针，不相遇则没环.相遇slow走一步，新开节点从头开始走，相遇时则为开始节点
+}
