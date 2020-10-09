02eecc92a0b7629bae6eb3775ce6181c0d4c050f
zzx <zhanxue.zhi@17zuoye.com>
2020-10-10 01:54:24 +0800

    二叉搜索树的最近公共祖先

---
+++ src/main/java/tree/二叉搜索树中节点的最近公共祖先_235/Solution.java
@@ -0,0 +1,60 @@
+package tree.二叉搜索树中节点的最近公共祖先_235;
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。”
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5]
+//
+//
+//
+//
+//
+// 示例 1:
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+//
+//
+// 示例 2:
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+//
+//
+//
+// 说明:
+//
+//
+// 所有节点的值都是唯一的。
+// p、q 为不同节点且均存在于给定的二叉搜索树中。
+//
+// Related Topics 树
+// 👍 466 👎 0
+
+import tree.structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+
+    //Time:O(logn)      Space:O(1)
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || p == null || q == null) return null;
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else return root;
+        }
+        return null;
+    }
+    //思路
+    //迭代    深度优先
+    //如果p、q都大于根节点root或者都小于根节点root，则移动root，否则当前根节点就是最近公公祖先
+
+}
