23abaecb72427915ab0bb073c1d829145f13fe5d
zzx <zhanxue.zhi@17zuoye.com>
2020-10-21 02:18:22 +0800

    单词拆分

---
+++ src/main/java/动态规划/字符串分解_139/Solution.java
@@ -0,0 +1,58 @@
+package 动态规划.字符串分解_139;
+//给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+//
+// 说明：
+//
+//
+// 拆分时可以重复使用字典中的单词。
+// 你可以假设字典中没有重复的单词。
+//
+//
+// 示例 1：
+//
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+//输出: true
+//解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+//
+//
+// 示例 2：
+//
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+//输出: true
+//解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+//     注意你可以重复使用字典中的单词。
+//
+//
+// 示例 3：
+//
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+//输出: false
+//
+// Related Topics 动态规划
+// 👍 717 👎 0
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+
+    //Time:O(n^2)       Space:O(n+m)
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dictSet = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && dictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+    //定义d(i)为前i个字符是否可以被拆分，定义j为[0,i-1]的一个分隔点，枚举j值，d(i) = d(j-1) && s.substring(j) in wordDict
+
+}
