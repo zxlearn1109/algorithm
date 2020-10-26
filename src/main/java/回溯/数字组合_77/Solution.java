e3e18c36dddd622a0152b9a7fba9dc69f560b643
zzx <zhanxue.zhi@17zuoye.com>
2020-10-26 12:44:51 +0800

    回溯.数字组合 题目说明

--- src/main/java/回溯/数字组合_77/Solution.java
+++ src/main/java/回溯/数字组合_77/Solution.java
@@ -3,6 +3,22 @@ package 回溯.数字组合_77;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+//给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+//
+// 示例:
+//
+// 输入: n = 4, k = 2
+//输出:
+//[
+//  [2,4],
+//  [3,4],
+//  [2,3],
+//  [1,2],
+//  [1,3],
+//  [1,4],
+//]
+// Related Topics 回溯算法
+// 👍 420 👎 0
 
 public class Solution {
 
