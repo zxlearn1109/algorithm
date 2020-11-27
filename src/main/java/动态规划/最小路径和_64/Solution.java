b215632340e5778f46e538d696c8f3258149826c
zzx <zhanxue.zhi@17zuoye.com>
2020-11-27 09:54:26 +0800

    dp.最小路径和

---
+++ src/main/java/动态规划/最小路径和_64/Solution.java
@@ -0,0 +1,59 @@
+package 动态规划.最小路径和_64;
+//给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+//
+// 说明：每次只能向下或者向右移动一步。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+//输出：7
+//解释：因为路径 1→3→1→1→1 的总和最小。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [[1,2,3],[4,5,6]]
+//输出：12
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 200 
+// 0 <= grid[i][j] <= 100 
+// 
+// Related Topics 数组 动态规划 
+// 👍 724 👎 0
+
+
+class Solution {
+
+    //Time: O(m * n)    Space:O(n)
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0
+                || grid[0] == null || grid[0].length == 0)
+            return 0;
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n];
+        dp[0] = grid[0][0];
+        for (int j = 1; j < n; j++) dp[j] = dp[j - 1] + grid[0][j];
+
+        for (int i = 1; i < m; i++) {
+            dp[0] += grid[i][0];
+            for (int j = 1; j < n; j++) {
+                dp[j] = Math.min(dp[j], dp[j - 1]) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+
+    //dp[i][j] = Math.min(dp[i-1)[j],dp[i][j-1]) + grid[i][j]
+}
